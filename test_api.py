af92d30212a079d7fe80f968590ae383694cb800
Benjamin Kampmann <ben.kampmann@gmail.com>
2012-12-25 19:03:22 +0100

    more profound api test script

--- test_api.py
+++ test_api.py
@@ -4,25 +4,54 @@ import urllib2
 import hashlib
 import hmac
 import binascii
+import requests
+import json
 
-key = "meinzwo"
-secret = "herforder weihnacht"
-params = {"yay": "other", "second": "yes"}
-URL = "http://localhost:9092/?"
+KEY = "ahFkZXZ-amVycnktc2VydmljZXIPCxIJQXBwQWNjZXNzGAEM"
+SECRET = "santa loves you"
+BASE_PATH = "/api/v1/"
+BASE_URL = "http://localhost:9092" + BASE_PATH
 
 
-def run():
-    params.update({
-            "_key": key
-        })
-    query = urllib.urlencode(params)
-    params["_signature"] = binascii.b2a_base64(
-            hmac.new(secret, query, hashlib.sha256).digest())
-    print URL, urllib.urlencode(params)
-    req = urllib2.urlopen(URL + urllib.urlencode(params))
+def _sign(method, url, params):
+    params["_key"] = KEY
+    encoded = urllib.urlencode(params)
+    query = "&".join((method.upper(), url, encoded))
+    encoded += "&_signature=" + urllib.quote(binascii.b2a_base64(
+            hmac.new(SECRET, query, hashlib.sha256).digest()))
+    return encoded
+
+
+def verify_access():
+    params = {"yay": "other", "second": "yes"}
+
+    url = BASE_URL + "verify_access"
+    full_request = url + "?" + _sign("GET", url, params)
+    print full_request
+    req = urllib2.urlopen(full_request)
+    print req.read()
+    return req
+
+
+def post_log_entry():
+
+    data = [
+        {"action": "upload_photo"},
+        {"action": "run_app", "quantity": 4},
+        {"action": "share_picture"}
+    ]
+    params = {
+        "device_id": "ASD90384KDOHDKS",
+        "entries": json.dumps(data)
+    }
+
+    url = BASE_URL + "logger"
+    req = requests.post(url, data=_sign("POST", url, params))
+    print req.content
+    print req.status_code
     import pdb
     pdb.set_trace()
-    print req.read()
 
 if __name__ == "__main__":
-    run()
+    verify_access()
+    post_log_entry()
