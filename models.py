9e57521eeec682ba7196d81bf3a30b02ba8ad14d
Benjamin Kampmann <ben.kampmann@gmail.com>
2012-12-25 19:03:42 +0100

    modeling v1

---
+++ models.py
@@ -0,0 +1,40 @@
+from google.appengine.ext import ndb
+
+
+class AppAccess(ndb.Model):
+    name = ndb.StringProperty()
+    active = ndb.BooleanProperty(default=True)
+    created = ndb.DateTimeProperty(auto_now_add=True)
+    secret = ndb.StringProperty(required=True, indexed=False)
+    domain = ndb.StringProperty(indexed=False)
+
+
+# for User Info
+class User(ndb.Expando):
+    # key = parent=>AppAccess; ID given to app
+    pass
+
+
+# for Device Info
+class Device(ndb.Expando):
+    # key = parent=>AppAccess; Device-ID given
+    pass
+
+
+class LogEntry(ndb.Model):
+    # key = parent=>AppAccess
+    when = ndb.DateTimeProperty("w", auto_now_add=True)
+    user = ndb.KeyProperty("us", kind=User, required=False)
+    device = ndb.KeyProperty("d", kind=Device, required=False)
+    action = ndb.StringProperty("a", required=True, indexed=True)
+    quantity = ndb.IntegerProperty("q", required=False, indexed=False, default=1)
+    unit = ndb.StringProperty("un", required=False, indexed=False)
+
+    @staticmethod
+    def log(cls, app_key, user_id, device_id, **params):
+        if user_id:
+            params["user"] = ndb.Key(User, user_id, parent=app_key)
+        if device_id:
+            params["device"] = ndb.Key(Device, device_id, parent=app_key)
+        params["parent"] = app_key
+        return cls(**params)
