021bcf5c35e4952ec202ac5e3a7090de11cee69d
Benjamin Kampmann <ben.kampmann@gmail.com>
2013-01-03 18:35:23 +0100

    add modal and restructure

--- static/js/console.js
+++ static/js/console.js
@@ -1,11 +1,40 @@
-var consoleServices = angular.module('consoleServices', ['ngResource']).
-    factory('App', function($resource){
-      return $resource('/api/v0/my_apps', {}, {
-    query: {method:'GET', params: {"_raw": 1}, isArray:true}
-  });
-});
+angular.module('console.services', ['ngResource']).
+  factory('App', function($resource){
+    return $resource('/api/v0/my_apps', {}, {
+      query: {method:'GET', params: {"_raw": 1}, isArray:true}
+    });
+  }).
+  directive('twModal', function() {
+    return {
+      scope: true,
+      link: function(scope, element, attr, ctrl) {
+          console.log(arguments);
+          scope.dismiss = function() {
+            $(element).modal("hide");
+          };
+        }
+      };
+  }).
+  service('appState', function(App, $rootScope){
+    var self = this;
+    self.selected_app = null;
 
-var consoleApp = angular.module('console', ["consoleServices"]).
+    self.selectApp = function(app) {
+      self.selected_app = app;
+    };
+
+    self.addApp = function(app) {
+      self.apps.push(app);
+    };
+
+    self.apps = App.query(function() {
+      // preselect first
+      self.selectApp(self.apps[0]);
+    });
+  })
+;
+
+var consoleApp = angular.module('console', ["console.services"]).
   config(function($routeProvider) {
 //    console.log($routeProvider);
 //     $routeProvider.
@@ -13,28 +42,23 @@ var consoleApp = angular.module('console', ["consoleServices"]).
 // //      when('/edit/:projectId', {controller:EditCtrl, templateUrl:'detail.html'}).
 // //      when('/new', {controller:CreateCtrl, templateUrl:'detail.html'}).
 //       otherwise({redirectTo:'/'});
+  }).
+  controller ("NavbarCtrl", function($scope, appState){
+    $scope.appState = appState;
+
+  }).
+  controller ("AddAppCtrl", function ($scope, App, appState) {
+    $scope.model = {};
+    console.log($scope);
+
+    $scope.saveApp = function() {
+      $scope.dismiss();
+      return;
+      var newApp = new App({name: $scope.model.app_name});
+      newApp.$save(function() {
+        console.log("yay");
+
+        appState.addApp(newApp);
+      });
+    };
   });
-consoleApp.value("appName", "consoleApp");
-
-consoleApp.controller("NavbarCtrl", function($scope, App) {
-  console.log($scope);
-  $scope.apps = App.query(function() {
-    // preselect first
-    $scope.selectApp($scope.apps[0]);
-  });
-  $scope.selectApp = function(app) {
-    $scope.selected_app = app;
-    console.log(app);
-  };
-}).
-controller ("AddAppCtrl", function ($scope, App) {
-
-  $scope.model = {};
-
-  $scope.saveApp = function() {
-    var newApp = new App({name: $scope.model.app_name});
-    newApp.$save();
-    console.log(newApp);
-  };
-  
-});
