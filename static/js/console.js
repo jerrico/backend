0d0249f3505f1a791d5b910485a64d3dd4ba3e51
Benjamin Kampmann <ben.kampmann@gmail.com>
2013-01-03 13:30:49 +0100

    add first edition of angular js

---
+++ static/js/console.js
@@ -0,0 +1,32 @@
+var consoleServices = angular.module('consoleServices', ['ngResource']).
+    factory('Apps', function($resource){
+  return $resource('/api/v0/my_apps', {}, {
+    query: {method:'GET', params: {"_raw": 1}, isArray:true}
+  });
+});
+
+var consoleApp = angular.module('console', ["consoleServices"]).
+  config(function($routeProvider) {
+//    console.log($routeProvider);
+//     $routeProvider.
+//       when('/', {controller:MainCtrl, templateUrl:'main.html'}).
+// //      when('/edit/:projectId', {controller:EditCtrl, templateUrl:'detail.html'}).
+// //      when('/new', {controller:CreateCtrl, templateUrl:'detail.html'}).
+//       otherwise({redirectTo:'/'});
+  });
+consoleApp.value("appName", "consoleApp");
+
+consoleApp.controller("NavbarCtrl", function($scope, Apps) {
+  console.log($scope);
+  $scope.apps = Apps.query(function() {
+    // preselect first
+    $scope.selectApp($scope.apps[0]);
+  });
+  $scope.selectApp = function(app) {
+    $scope.selected_app = app;
+    console.log(app);
+  };
+}).
+controller ("MainCtrl", function ($scope, Apps) {
+  
+});
